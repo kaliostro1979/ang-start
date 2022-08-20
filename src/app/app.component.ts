0e7d830a4c53ed9d9d6b94a7eb4668737db3ef9b
Mikayel <abachyanm@gmail.com>
2022-08-21 02:15:40 +0400

    lesson-7 block-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'start-project';
+  text = ""
+
+  setText = (event: Event)=>{
+    this.text = (<HTMLInputElement>event.target).value
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {FirstComponent} from "./first/first.component";
-import { SecondComponent } from './second/second.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FirstComponent,
-    SecondComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
